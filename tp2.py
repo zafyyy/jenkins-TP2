1f080800f51e1ad739e162caa9edb23bcd2a2100
sara <norosoa.martin@gmail.com>
2021-10-05 09:57:50 +0200

    ajout branche py2

--- tp2.py
+++ tp2.py
@@ -1,8 +1,8 @@
-print("combien de temps vous avez vécu")
+print("combien de temps vous avez vécu:")
 nom = input("nom:")
 print("donnez votre age:")
 age = int (input ("age:"))
 jour = age * 365
 minutes = age * 525948
 secondes = age * 31556926
-print (nom, "a vecu", "j", minutes, "mn et ", secondes, "s")
+print (nom, "a vecu", "jours", minutes, "mn et ", secondes, "s")
