fecd3cc5c411e715aedbdec53f2c936ebf1e0b5a
sara <norosoa.martin@gmail.com>
2021-10-05 09:53:34 +0200

    test1

---
+++ tp2.py
@@ -0,0 +1,8 @@
+print("combien de temps vous avez vécu")
+nom = input("nom:")
+print("donnez votre age:")
+age = int (input ("age:"))
+jour = age * 365
+minutes = age * 525948
+secondes = age * 31556926
+print (nom, "a vecu", "j", minutes, "mn et ", secondes, "s")
